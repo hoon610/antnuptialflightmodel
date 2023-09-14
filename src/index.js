0bb9cc280d8a945cbb40e1239555c642b66e394f
hoon610 <bhoonkang@gmail.com>
2023-09-14 15:44:19 -0400

    Create basic template for weatherAPI requests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import './style.css';
 
 
-console.log("works")
+const searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', function() {
+    searchWeather();
+});
+
+
+async function searchWeather() {
+    const locationInput = document.getElementById('locationInput').value;
+    const weatherData = await fetchWeather(locationInput);
+    const processedData = processData(weatherData);
+    displayWeather(processedData);
+}
+
+async function fetchWeather(location){
+    const apiKey = '1cd253fc61af4715bf3191326231808';
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`, { mode: 'cors' });
+    const rawWeatherData = await response.json();
+    return rawWeatherData;
+}
+
+function processData(){
+    console.log("hi")
+}
+function displayWeather(){
+    
+}
+
+
+console.log("hello")
+
